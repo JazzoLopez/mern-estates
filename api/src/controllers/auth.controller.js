6a252531ac1f9b3d9704bed5f1431b285444c484
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-06-05 12:49:52 -0600

    Fix: error message

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -5,13 +5,32 @@ const authController = {};
 
 authController.signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  //validar que no vengan vacios
+  if (!username || !email || !password) {
+    const error = new Error("Username, email and password are required");
+    error.statusCode = 400;
+    return next(error);
+  }
+  const isExist = await userModel.findOne({ username: username });
+  if (isExist) {
+    const error = new Error("Username already exist");
+    error.statusCode = 400;
+    return next(error);
+  }
+
+  if(password.length <8){
+    const error = new Error("Password must be at least 8 characters long");
+    error.statusCode = 400;
+    return next(error)
+  }
   const hashedPassword = bcrypt.hashSync(password, 10);
+
   const newUser = new userModel({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({
       "success": true,
-      "message": "User created successfully"
+      "error": "User created successfully"
     });
   } catch (error) {
     next(error);
