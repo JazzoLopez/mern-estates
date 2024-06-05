33a8ccb47f206a63a87203fbd38a2d78675ab27f
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-06-05 12:34:55 -0600

    Fix: auth

--- api/src/app.js
+++ api/src/app.js
@@ -3,9 +3,11 @@ import morgan from 'morgan';
 import userModel from './models/user.model.js';
 import authRoutes from './routes/auth.routes.js';
 import { config } from 'dotenv';
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -4,15 +4,18 @@ import bcrypt from 'bcrypt';
 const authController = {};
 
 authController.signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new userModel({ username, email, password: hashedPassword });
-    try {
-      await newUser.save();
-      res.status(201).json('User created successfully!');
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new userModel({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({
+      "success": true,
+      "message": "User created successfully"
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default authController;
