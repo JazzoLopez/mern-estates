67096d14ce92a7b147da2dc9ae1821d4aaf0b3f1
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-06-05 12:49:34 -0600

    Fix: signin

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>SignIn
+      <div className='gap-2 flex mt-5'>
+        <p>no account?</p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
+      </div>
+    </div>
+    
   )
 }
